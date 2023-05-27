000759083a81e0fd0899aa6ee92acd913bf41d1d
Joffre <joffre_quintas@hotmail.com>
2023-05-27 11:00:26 -0300

    Página de login estruturada.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,28 @@
+import Button from "../../components/Button/Button";
+
 export default function Login() {
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log('test')
+    }
+
     return(
         <div className="container">
-            <h1>Login</h1>
+            <form>
+                <h1>Login</h1>
+                <div>
+                    <label htmlFor="user">Usuário</label>
+                    <input type="text" name="user"/>
+                </div>
+                <div>
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" name="password"/>
+                </div>
+                <div>
+                    <Button title="Entrar" onClick={(e) => handleSubmit(e)}></Button>
+                </div>
+            </form>
         </div>
     )
 }
