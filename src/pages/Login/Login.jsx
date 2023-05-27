1c79947eec1d8889c06cc164b2d8268efb1540c6
Joffre <joffre_quintas@hotmail.com>
2023-05-27 14:15:27 -0300

    Criado o component de input e estilização basica da pagina do login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,53 @@
+import style from './Login.module.scss';
+
 import Button from "../../components/Button/Button";
+import Input from '../../components/Input/Input';
+import { AiFillEye } from 'react-icons/ai'
+import { AiFillEyeInvisible } from 'react-icons/ai'
+
+import { useState } from "react";
+
 
 export default function Login() {
+    const [type, setType] = useState('password')
+    const [form, setForm] = useState({
+        user:'',
+        password: ''
+    })
 
+    //Function to submit form
     function handleSubmit(e) {
         e.preventDefault()
-        console.log('test')
+        console.log(form)
+    }
+
+    //Function do update form
+    function handleUpdateInput(e) {
+        const fieldCamp = e.target.name
+        const fieldValue = e.target.value
+        setForm((currentForm) => {
+            return{
+                ...currentForm,
+                [fieldCamp]: fieldValue
+            }
+        })
     }
 
     return(
         <div className="container">
-            <form>
+            <form className={style.formLogin}>
                 <h1>Login</h1>
-                <div>
-                    <label htmlFor="user">Usuário</label>
-                    <input type="text" name="user"/>
+                <Input label='Usuário' type='text' name='user' id='user' onChange={(e) => handleUpdateInput(e)}/>
+                <div className={`${style.divInput} ${style.divPassword}`}>
+                    <input type={type} name="password" onChange={(e) => handleUpdateInput(e)} placeholder=' ' id='password'/>
+                    <label htmlFor="password">Senha</label>         
+                    {type == 'password' ? <AiFillEyeInvisible onClick={() => setType
+                    ('text')}/> : <AiFillEye onClick={() => setType('password')}/>}
                 </div>
-                <div>
-                    <label htmlFor="password">Senha</label>
-                    <input type="password" name="password"/>
+                <div className={style.haventLogin}>
+                    <p>Ainda não possui cadastro? <span>Clique aqui</span></p>
                 </div>
-                <div>
+                <div className={style.actions}>
                     <Button title="Entrar" onClick={(e) => handleSubmit(e)}></Button>
                 </div>
             </form>
