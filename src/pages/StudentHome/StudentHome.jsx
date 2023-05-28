55c352953c65fd3bc16b6292eacd0d21489c9080
Joffre <joffre_quintas@hotmail.com>
2023-05-27 22:11:59 -0300

    Ajustes visuais na página do estudante.

--- src/pages/StudentHome/StudentHome.jsx
+++ src/pages/StudentHome/StudentHome.jsx
@@ -1,23 +1,25 @@
 import Caroussel from "../../components/Caroussel/Caroussel"
+import Button from '../../components/Button/Button';
+
+import style from './StudentHome.module.scss';
 
 export default function StudentHome() {
     
 
-
     return(
-        <div>
+        <div className="container">
             <h1>Olá, ser humano!</h1>
-            <section>
+            <section className={style.playWhereStoped}>
                 <p>Continue de onde parou:</p>
                 <p>imagem</p>
                 <p>Módulo 01 - Construção</p>
                 <p>Aula 03 - Entendendo o Grid</p>
-                <button>Continuar</button>
+                <Button title='Continuar'/>
             </section>
-            <section>
+            <section className={style.modules}>
                 <h3>Módulos</h3>
                 <Caroussel/>
-                <button>Ver trilha completa</button>
+                <Button className='buttonFit' title='Ver trilha completa'/>
             </section>
         </div>
     )
